b23f42a8e8208ab496c3fdb48c1078f9045ce129
Ben DeSollar <ben.desollar01@gmail.com>
2023-03-06 21:09:01 -0600

    Python update

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=8000)
+    app.run(port=8000)
